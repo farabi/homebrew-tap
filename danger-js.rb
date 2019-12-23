687b12648a7691caecf8c430cdd5029aaa05f3dc
Saad El Oulladi <saad.eloulladi@gmail.com>
2019-12-23 23:06:45 +0100

    Update danger-js.rb

--- danger-js.rb
+++ danger-js.rb
@@ -1,6 +1,6 @@
 class DangerJs < Formula
   homepage "https://github.com/farabi/danger-js"
-  url "https://github.com/farabi/danger-js/releases/download/9.1.5/danger-macos.zip"
+  url "https://github.com/danger/danger-js/releases/download/9.1.5/danger-macos.zip"
   sha256 "33a4a48875070291348333dcc477ef653cec4c35ea9ec09e17f9d22ac767d8a2"
 
   def install
