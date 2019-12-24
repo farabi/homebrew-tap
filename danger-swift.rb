eaa9d5b27a9c32579c4dd408e2de3bec4bf9f6dd
Saad El Oulladi <saad.eloulladi@gmail.com>
2019-12-24 12:53:33 +0100

    Test

--- danger-swift.rb
+++ danger-swift.rb
@@ -1,9 +1,7 @@
 class DangerSwift < Formula
   desc "Write your Dangerfiles in Swift"
-  homepage "https://github.com/farabi/danger-swift"
-  url "https://github.com/farabi/danger-swift"
+  url "https://github.com/farabi/danger-swift.git"
   sha256 "8dd28a175e631cba46f4b44c0656cbbf04a6beac9655283904dcf96ad6d95860"
-  head "https://github.com/farabi/danger-swift.git"
 
   # Runs only on Xcode 10
   depends_on :xcode => ["10", :build]
