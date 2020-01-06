02a68a641ec87f0243de046fa77904192415647d
Saad El Oulladi <saad.eloulladi@gmail.com>
2020-01-06 14:06:30 +0100

    Update danger-js.rb
    Update checksum

--- danger-js.rb
+++ danger-js.rb
@@ -1,7 +1,7 @@
 class DangerJs < Formula
   homepage "https://github.com/farabi/danger-js"
   url "https://github.com/farabi/danger-js/releases/download/2.3.0/danger.zip"
-  sha256 "842b4622cf2e857a49b4dfade7acaf9ee6f34d6e98203b8bc24b628308329fe5"
+  sha256 "6755438665fbace1673da62fe092e61129f5cced4b7f75877f9f88ddac0b6414"
 
   def install
     bin.install "danger"
