1853a8b0baf4ef190cc71cf69d4b348522c1e3a5
Saad El Oulladi <saad.eloulladi@gmail.com>
2020-01-20 22:57:28 +0100

    Set danger js repo

--- danger-js.rb
+++ danger-js.rb
@@ -1,6 +1,7 @@
 class DangerJs < Formula
   homepage "https://github.com/farabi/danger-js"
-  url "https://github.com/farabi/danger-js/releases/download/2.3.0/danger.zip"
+  #url "https://github.com/farabi/danger-js/releases/download/2.3.0/danger.zip"
+  url "https://github.com/farabi/DangerDemo/releases/download/2.3.0/danger.zip"
   sha256 "c593d7395477462c736150c7785cdfe379215f769bb56db4a16248dfe21793c8"
 
   def install
